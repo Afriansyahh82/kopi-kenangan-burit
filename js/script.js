24100d372b07a34b0939410da6b0dbb84607ebbc
Afriansyahh82 <afriansyahidris44@gmail.com>
2024-07-13 20:55:44 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
-// TOGGLE CLASS ACTIVE
+// TOGGLE CLASS ACTIVE UNTUK HAMBURGER MENU
 const navbarNav = document.querySelector(".navbar-nav");
 // KETIKA HAMBURGER MENU DI KLIK
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// KLIK DI LUAR SIDE BAR UNTUK MENGHILANGKAN NAV
-const hamburger = document.querySelector("#hamburger-menu");
+// TOGGLE CLASS ACTIVE UNTUK SEARCH FORM
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// TOGGLE CLASS ACTIVE UNTUK SHOPPING CART
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// KLIK DI LUAR ELEMEN
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// MODAL BOX
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// KLIK TOMBOL CLOSE
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// KLIK DILUAR MODAL
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
