8493355e2376185c560c3c18e8dc83aff5369fad
Afriansyahh82 <afriansyahidris44@gmail.com>
2024-08-17 20:12:17 +0700

    Fitur Shopping cart

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
-      { id: 1, name: "Robusta Brazil", img: "1", price: 20000 },
-      { id: 2, name: "Arabica Blend", img: "2", price: 25000 },
-      { id: 3, name: "Primo Passo", img: "3", price: 30000 },
-      { id: 4, name: "Aceh Gayo", img: "4", price: 35000 },
-      { id: 5, name: "Sumatra Mandheling", img: "5", price: 40000 },
+      { id: 1, name: "Robusta Brazil", img: "1.jpg", price: 20000 },
+      { id: 2, name: "Arabica Blend", img: "2.jpg", price: 25000 },
+      { id: 3, name: "Primo Passo", img: "3.jpg", price: 30000 },
+      { id: 4, name: "Aceh Gayo", img: "4.jpg", price: 35000 },
+      { id: 5, name: "Sumatra Mandheling", img: "5.jpg", price: 40000 },
     ],
   }));
 
